5105dd395a67df371a644f1bffc8f40c6176d49f
shyamansi <66917662+shyamansi@users.noreply.github.com>
2025-03-14 13:32:53 +0530

    second failed test

--- spec/NumberString/number_calculator_spec.rb
+++ spec/NumberString/number_calculator_spec.rb
@@ -5,5 +5,8 @@ require 'rails_helper'
       it 'returns 0 for an empty string' do
         expect(NumberCalculator.new.add("")).to eq(0)
       end
+      it 'returns the number itself for a single number' do
+        expect(NumberCalculator.new.add("5")).to eq(5)
+      end
     end
   end
