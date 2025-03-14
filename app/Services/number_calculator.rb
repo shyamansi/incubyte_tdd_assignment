cb73f9e85ffa57f67ac67d1b49dc365816882f3b
shyamansi <66917662+shyamansi@users.noreply.github.com>
2025-03-14 14:59:41 +0530

    Sixth passed test

--- app/Services/number_calculator.rb
+++ app/Services/number_calculator.rb
@@ -1,6 +1,11 @@
 class NumberCalculator
 	def add(num)
 		return 0 if num.empty?
-		 return num = num.gsub("\n", ",").split(",").map(&:to_i).sum
+		if num.start_with?("//")
+    delimiter, num = num[2..].split("\n", 2)
+    num = num.gsub(delimiter, ",")
+  end
+
+  num = num.gsub("\n", ",").split(",").map(&:to_i).sum
 	end
 end
