bf049bfe1811ddb8bb3e70389e22f2b8a5856f9f
shyamansi <66917662+shyamansi@users.noreply.github.com>
2025-03-14 14:37:56 +0530

    Sixth failed test

--- spec/NumberString/number_calculator_spec.rb
+++ spec/NumberString/number_calculator_spec.rb
@@ -17,5 +17,8 @@ require 'rails_helper'
        it 'handles newlines as delimiters' do
         expect(NumberCalculator.new.add("1\n2,3")).to eq(6)
       end
+      it 'handles custom delimiters' do
+        expect(NumberCalculator.new.add("//;\n1;2")).to eq(3)
+      end
     end
   end
