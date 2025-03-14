54cf03d737ee6238e80a524d8ead5dbab4a400c7
shyamansi <66917662+shyamansi@users.noreply.github.com>
2025-03-14 14:05:59 +0530

    Fourth passed test

--- spec/NumberString/number_calculator_spec.rb
+++ spec/NumberString/number_calculator_spec.rb
@@ -11,5 +11,8 @@ require 'rails_helper'
       it 'returns the sum of two comma-separated numbers' do
        expect(NumberCalculator.new.add("1,2")).to eq(3)
       end
+       it 'returns the sum of multiple comma-separated numbers' do
+        expect(NumberCalculator.new.add("1,2,3,4")).to eq(10)
+      end
     end
   end
