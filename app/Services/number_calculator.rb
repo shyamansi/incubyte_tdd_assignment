e7f5ebfeb02d1c4f8abd5d0fd21951c8dd9c796d
shyamansi <66917662+shyamansi@users.noreply.github.com>
2025-03-14 13:55:12 +0530

    Third failed test

--- app/Services/number_calculator.rb
+++ app/Services/number_calculator.rb
@@ -1,6 +1,7 @@
 class NumberCalculator
 	def add(num)
 		return 0 if num.empty?
-		return num.to_i if num
+		#return num.to_i if num
+		return num.split(',').map(&:to_i).sum
 	end
 end
--- spec/NumberString/number_calculator_spec.rb
+++ spec/NumberString/number_calculator_spec.rb
@@ -8,5 +8,8 @@ require 'rails_helper'
       it 'returns the number itself for a single number' do
         expect(NumberCalculator.new.add("5")).to eq(5)
       end
+      it 'returns the sum of two comma-separated numbers' do
+       expect(NumberCalculator.new.add("1,2")).to eq(3)
+      end
     end
   end
